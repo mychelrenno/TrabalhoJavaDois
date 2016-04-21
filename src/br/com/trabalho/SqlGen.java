73ee993797fa1d364cb8f0279b61e9ef22e1b2a6
Mychel Rennó <Mychel Renn@DESKTOP-NS0GKA9>
2016-04-20 21:53:18 -0300

    commit treis

---
+++ src/br/com/trabalho/SqlGen.java
@@ -0,0 +1,16 @@
+package br.com.trabalho;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public abstract class SqlGen {
+	// DDL
+	protected abstract String getCreateTable(Connection con, Object obj);
+	protected abstract String getDropTable(Connection con, Object obj);
+	// DML
+	protected abstract PreparedStatement getSqlInsert(Connection con, Object obj);
+	protected abstract PreparedStatement getSqlSelectAll(Connection con, Object obj);
+	protected abstract PreparedStatement getSqlSelectById(Connection con, Object obj);
+	protected abstract PreparedStatement getSqlUpdateById(Connection con, Object obj);
+	protected abstract PreparedStatement getSqlDeleteById(Connection con, Object obj);
+}
