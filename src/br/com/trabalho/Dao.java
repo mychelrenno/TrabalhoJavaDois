365de0e8cf34aac48558dd04e80aa54243984848
Mychel Rennó <Mychel Renn@192.168.25.3>
2016-04-21 14:42:32 -0300

    commint sete

--- src/br/com/trabalho/Dao.java
+++ src/br/com/trabalho/Dao.java
@@ -3,9 +3,14 @@ package br.com.trabalho;
 import java.util.List;
 
 public interface Dao<T, K> {
-public void salvar(T t);
-public T buscar(K k);
-public void atualizar(T t);
-public void excluir(K k);
-public List<T> listarTodos();
+	
+	public void salvar(T t);
+	
+	public T buscar(K k);
+	
+	public void atualizar(T t);
+	
+	public void excluir(K k);
+	
+	public List<T> listarTodos();
 }
---
+++ src/br/com/trabalho/Main.java
@@ -0,0 +1,8 @@
+package br.com.trabalho;
+
+public class Main {
+
+	public static void main (String []args){
+		
+	}
+}
