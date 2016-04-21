bbc703ace2668727de40ee5d69eb0292b1daa428
Mychel Rennó <Mychel Renn@192.168.25.3>
2016-04-21 15:10:10 -0300

    commit oito

--- src/br/com/trabalho/Main.java
+++ src/br/com/trabalho/Main.java
@@ -2,6 +2,46 @@ package br.com.trabalho;
 
 public class Main {
 
+	public void apagarTabela(){
+		
+	}
+
+	public void criarTabela(){
+		
+	}
+
+	public void inserir (Object um){
+		
+	}
+
+	public void inserirDois (Object dois){
+		
+	}
+
+	public void inserirTres (Object tres){
+		
+	}
+
+	public void listarTodos (){
+		
+	}
+
+	public void buscar (Object um){
+		
+	}
+
+	public void alterar (Object um){
+		
+	}
+
+	public void excluir (Object dois){
+		
+	}
+
+	public void listarTodosDois(){
+		
+	}
+	
 	public static void main (String []args){
 		
 	}
