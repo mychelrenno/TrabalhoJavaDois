5db1c5fff2e40f37ab83179629e971676754cc1b
Mychel Rennó <Mychel Renn@DESKTOP-NS0GKA9>
2016-04-20 21:57:16 -0300

    commit quatro

--- src/br/com/trabalho/Cliente.java
+++ src/br/com/trabalho/Cliente.java
@@ -8,4 +8,35 @@ public class Cliente {
 	private String telefone;
 	private EstadoCivil estadoCivil;
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public EstadoCivil getEstadoCivil() {
+		return estadoCivil;
+	}
+	public void setEstadoCivil(EstadoCivil estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+	
 }
